3f383335ae93cada76b72c440a1e227e97a23e25
sgwealti <shanewealti@gmail.com>
2016-01-16 18:19:38 -0500

    Added tests
    Added tests.

--- test/clojure_caves/core_test.clj
+++ test/clojure_caves/core_test.clj
@@ -1,7 +1,28 @@
 (ns clojure-caves.core-test
   (:require [clojure.test :refer :all]
-            [clojure-caves.core :refer :all]))
+            [clojure-caves.core :refer :all])
+  (:import [clojure-caves.core UI World Game])
+  (:use clojure.test
+        clojure-caves.core))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+
+(defn current-ui [game]
+  (:kind (last (:uis game))))
+
+
+(deftest test-start
+  (let [game (new Game nil [(new UI :start)] nil)]
+
+    (testing "Enter wins at the starting screen."
+      (let [result (process-input game :enter)]
+        (is (= (current-ui result) :win))))
+
+    (testing "Other keys lose at the starting screen."
+      (let [results (map (partial process-input game)
+                         [\space \a \A :escape :up :backspace])]
+        (doseq [result results]
+          (is (= (current-ui result) :lose)))))))
