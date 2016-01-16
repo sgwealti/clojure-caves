dada7468c21ab44263351da335ac854323e7384a
sgwealti <shanewealti@gmail.com>
2016-01-16 16:58:56 -0500

    Caves of clojure part 2.
    Caves of clojure part 2.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clojure-caves "0.1.0-SNAPSHOT"
+  :description "Shane's Caves of Clojure"
+  :url "N/A"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clojure-lanterna "0.9.0"]]
+  :main clojure-caves.core)
+
